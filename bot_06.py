c87dc6f5cd46f23ef6c0d0d04dc6a77f91870b69
Paulo <paulovsp16@hotmail.com>
2020-12-28 21:40:21 -0300

    Adicionado pasta com imagens que o bot irá aprender as cordenadas. Melhorias no código.

--- bot_06.py
+++ bot_06.py
@@ -5,29 +5,30 @@ import rpa as r
 import pyscreeze as py
 import pandas as pd
 import os
+from pathlib import Path
 
 
 def main():
     url = 'https://rpachallengeocr.azurewebsites.net/'
-    accessible_url = conection_url_verify(url)
+    accessible_url = connection_url_verify(url)
 
     site_init(url) if accessible_url else print("Bye.")
 
-    max_window_img = 'max_window.png'
-    move_to_center_img_click(max_window_img)
+    max_window_img = img_path('max_window.png')
+    move_to_center_img_click(str(max_window_img))
 
     p.sleep(1)
     p.hotkey('tab')
     p.press('right', presses=10)
 
-    p.sleep(1.5)
-    next_btn_img = 'next.png'
-    extract_table_from_page(next_btn_img)
+    p.sleep(0.5)
+    next_btn_img = img_path('next.png')
+    extract_table_from_page(str(next_btn_img))
 
     r.close()
 
 
-def conection_url_verify(url_to_be_verify):
+def connection_url_verify(url_to_be_verify):
     try:
         url = urllib.request.urlopen(url_to_be_verify)
     except (urllib.error.URLError):
@@ -44,25 +45,30 @@ def site_init(url):
     r.url(webpage_url=url)
 
 
+def img_path(img_name):
+    file_path = Path('bot_udemy_git/pv/images') # path to /images 
+    return file_path / img_name
+
+
 def extract_table_from_page(next_image):
     for count in range(1, 4):
         if count == 1:
-            r.table('//*[@id="tableSandbox"]', 'Temp.csv')
-            extrc_table = pd.read_csv('Temp.csv')
-            extrc_table.to_csv(r'WebTable.csv', mode='a', index=None, header=True)
+            r.table('//*[@id="tableSandbox"]', 'temp.csv')
+            extrc_table = pd.read_csv('temp.csv')
+            extrc_table.to_csv(r'webtable.csv', mode='a', index=None, header=True)
             move_to_center_img_click(next_image, 0.7)
 
         else:
-            r.table('//*[@id="tableSandbox"]/tbody', 'Temp.csv')
-            extrc_table = pd.read_csv('Temp.csv')
-            extrc_table.to_csv(r'WebTable.csv', mode='a', index=None, header=True)
+            r.table('//*[@id="tableSandbox"]/tbody', 'temp.csv')
+            extrc_table = pd.read_csv('temp.csv')
+            extrc_table.to_csv(r'webtable.csv', mode='a', index=None, header=True)
             move_to_center_img_click(next_image, 0.7)
         # os.remove('Temp.csv')
 
 
-def move_to_center_img_click(image, conf=0.5):
-    cordinate = center_image(image, conf)
-    p.moveTo(cordinate, duration=1)
+def move_to_center_img_click(img, conf=0.5):
+    cordinate = center_image(img, conf)
+    p.moveTo(cordinate, duration=0.4)
     p.sleep(2)
     p.click()
 
